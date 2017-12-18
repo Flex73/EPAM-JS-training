4f0b5f9b02c8f535264a7fb58b3a6a2323bf768c
Flex73 <flex7398@gmail.com>
2017-12-18 15:50:57 +0300

    Add files via upload

--- todo-app/src/App.test.js
+++ todo-app/src/App.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
+
+
+const store = createStore(reducer);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
